08af2c92fc8dde2e2c4ff66b0f9bcb60e778b49c
Deirdre Connolly <durumcrustulum@gmail.com>
2022-02-28 22:51:27 -0500

    revert extra disks

--- docker/bridge/create-instance-template.sh
+++ docker/bridge/create-instance-template.sh
@@ -6,8 +6,6 @@ NAME="tor-obfsv4-bridge-$ID"
 gcloud compute instance-templates create-with-container $NAME \
   --container-image europe-docker.pkg.dev/onions-342500/onions/bridge:latest \
   --container-env OR_PORT="8080",PT_PORT="80",EMAIL="" \
-  --container-mount-disk mount-path="/var/lib/tor",name=$NAME \
-  --create-disk name=$NAME,size=25GB,type=pd-ssd \
   --machine-type n2d-standard-2 \
   --shielded-secure-boot \
   --tags allow-8080,http-server,https-server
