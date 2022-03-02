8d92447b47f57e8bf82c5ec7dc5e2adadbdf4b73
Deirdre Connolly <durumcrustulum@gmail.com>
2022-03-02 01:25:15 -0500

    Add relay Dockerfile and entry

---
+++ docker/relay/start-tor.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+NICK=${NICKNAME:-RANDOM}
+
+echo "Using NICKNAME=${NICK}, OR_PORT=${OR_PORT}."
+
+cat > /etc/tor/torrc << EOF
+Nickname ${NICK}
+ORPort ${OR_PORT}
+ExitRelay 0
+SocksPort 0
+EOF
+
+echo "Starting tor."
+tor -f /etc/tor/torrc
